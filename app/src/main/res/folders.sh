64ffc129a5f77efe18d4a1650b23dadaf5a65ece
Benjamin P Lee <benjaminplee@gmail.com>
2016-04-26 10:51:48 -0400

    Automating movement of files

--- app/src/main/res/folders.sh
+++ app/src/main/res/folders.sh
@@ -4,7 +4,14 @@ for dpi in ldpi mdpi hdpi xhdpi xxhdpi xxxhdpi; do
   for land in port land; do
     for size in small medium large; do
       folder="drawable-$dpi-$land-$size"
-      echo $folder
+      echo "-----" $folder "-----"
+
+      rm -rf $folder
+      mkdir $folder
+
+      cp "drawable-$dpi/solo_dpi.png" $folder/dpi.png
+      cp "drawable-$land/solo_orient.png" $folder/orient.png
+      cp "drawable-$size/solo_size.png" $folder/size.png
     done
   done
 done

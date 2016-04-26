8969fb4d7d66e779161697d71c01d16150175291
Benjamin P Lee <benjaminplee@gmail.com>
2016-04-26 11:13:37 -0400

    Moved folder names to be correct

--- app/src/main/res/folders.sh
+++ app/src/main/res/folders.sh
@@ -1,16 +1,16 @@
 #! /usr/bin/env bash
 
 for dpi in ldpi mdpi hdpi xhdpi xxhdpi xxxhdpi; do
-  for land in port land; do
+  for orient in port land; do
     for size in small medium large; do
-      folder="drawable-$dpi-$land-$size"
+      folder="drawable-$size-$orient-$dpi"
       echo "-----" $folder "-----"
 
       rm -rf $folder
       mkdir $folder
 
       cp "drawable-$dpi/solo_dpi.png" $folder/dpi.png
-      cp "drawable-$land/solo_orient.png" $folder/orient.png
+      cp "drawable-$orient/solo_orient.png" $folder/orient.png
       cp "drawable-$size/solo_size.png" $folder/size.png
     done
   done
